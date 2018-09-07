b0aeabb5ef28935e17997547255b19a1e2243753
coderdream <Xulin201486>
2018-09-07 16:27:04 +0800

    modify ch06

--- ch05/Gruntfile.js
+++ ch05/Gruntfile.js
@@ -12,11 +12,13 @@ module.exports = function(grunt){
 	// configure plugins
 	grunt.initConfig({
 		cafemocha: {
-			all: { src: 'qa/tests-*.js', options: { ui: 'tdd' }, }
+			all: { src: 'qa/tests-unit.js', options: { ui: 'tdd' }, }
 		},
 		jshint: {
-			app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
-			qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
+			files: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js','Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
+			options: {
+			    esnext: true
+            }
 		},
 		exec: {
 			linkchecker: { cmd: 'linkchecker http://localhost:3000' }
--- ch05/qa/tests-crosspage.js
+++ ch05/qa/tests-crosspage.js
@@ -1,42 +1,56 @@
 let Browser = require('zombie'),
-	assert = require('chai').assert;
+    assert = require('chai').assert;
 
 let browser;
 
-suite('Cross-Page Tests', function(){
+suite('Cross-Page Tests', function () {
 
-	setup(function(){
-		browser = new Browser();
-	});
+    setup(function () {
+        browser = new Browser();
+    });
 
-	test('requesting a group rate quote from the hood river tour page should ' +
-			'populate the hidden referrer field correctly', function(done){
-		let referrer = 'http://localhost:3000/tours/hood-river';
-		browser.visit(referrer, function(){
-			browser.clickLink('.requestGroupRate', function(){
-				assert(browser.field('referrer').value === referrer);
-				done();
-			});
-		});
-	});
+    test('requesting a group rate quote from the hood river tour page should ' +
+        'populate the hidden referrer field correctly', function (done) {
+        let referrer = 'http://localhost:3000/tours/hood-river';
+        browser.visit(referrer, function () {
+            browser.clickLink('.requestGroupRate', function () {
+                //
+                // browser.window.$(function(){
+                //     setTimeout(()=>{
+                //         browser.fill('referrer', 'user');
+                //         let ddd=browser.field('referrer').value;
+                //         debugger;
+                //     },10000);
+                //
+                // });
+                // let b = browser.source;
+                // console.log(b);
+                console.log('browser:  ' + browser.field('referrer').value);
+                console.log('referrer: ' + referrer);
+                assert(browser.field('referrer').value === referrer);
+            });
 
-	test('requesting a group rate from the oregon coast tour page should ' +
-			'populate the hidden referrer field correctly', function(done){
-		let referrer = 'http://localhost:3000/tours/oregon-coast';
-		browser.visit(referrer, function(){
-			browser.clickLink('.requestGroupRate', function(){
-				assert(browser.field('referrer').value === referrer);
-				done();
-			});
-		});
-	});
+           // done();
+        });
+    });
 
-	test('visiting the "request group rate" page dirctly should result ' +
-			'in an empty value for the referrer field', function(done){
-		browser.visit('http://localhost:3000/tours/request-group-rate', function(){
-			assert(browser.field('referrer').value === '');
-			done();
-		});
-	});
+    test('requesting a group rate from the oregon coast tour page should ' +
+        'populate the hidden referrer field correctly', function (done) {
+        let referrer = 'http://localhost:3000/tours/oregon-coast';
+        browser.visit(referrer, function () {
+            browser.clickLink('.requestGroupRate', function () {
+                assert(browser.field('referrer').value === referrer);
+                done();
+            });
+        });
+    });
+
+    test('visiting the "request group rate" page dirctly should result ' +
+        'in an empty value for the referrer field', function (done) {
+        browser.visit('http://localhost:3000/tours/request-group-rate', function () {
+            assert(browser.field('referrer').value === '');
+            done();
+        });
+    });
 
 });
